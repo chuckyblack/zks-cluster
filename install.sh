ac579609289b93efe0d5f0dc28726211c446f574
chuckyblack <jaromir.pufler@gmail.com>
2016-02-09 10:20:33 +0100

    debian/ubuntu version independent source.list

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 apt-get purge lxc-docker*
 apt-get purge docker.io*
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-printf "deb https://apt.dockerproject.org/repo debian-jessie main" >> /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo $(lsb_release -si | awk '{print tolower($0)}')-$(lsb_release -sc) main" > /etc/apt/sources.list.d/docker.list
 apt-get install -y apt-transport-https
 apt-get update
 # apt-cache policy docker-engine
